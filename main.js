882cf0aaaa0be885ca4aff6ba2b26e9bec573b67
MBriese <mindi.briese@gmail.com>
2024-02-11 12:20:10 -0800

    fetching pet data from db

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
     const weatherData = await weatherPromise.json()
-    const ourTemperature = weatherData.properties.periods[0].temperature
+    ourTemperature = weatherData.properties.periods[0].temperature;
     document.querySelector("#temperature-output").textContent = ourTemperature
 }
 
 start().then()
+
+async function  petsArea() {
+const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
+    const petsData = await petsPromise.json()
+    petsData.forEach(pet => {
+        console.log(pet.species)
+    })
+}
+
+petsArea().then()
