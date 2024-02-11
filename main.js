5d7501dc38c5baec96d6223d971635c21d774ecc
MBriese <mindi.briese@gmail.com>
2024-02-11 13:14:46 -0800

    added pet name to dynamic pet card

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+const template = document.querySelector('#pet-card-template')
+const wrapper = document.createDocumentFragment()
+
 async function start() {
     const weatherPromise = await fetch("https://api.weather.gov/gridpoints/MFL/110,50/forecast")
     const weatherData = await weatherPromise.json()
@@ -11,8 +14,12 @@ async function  petsArea() {
 const petsPromise = await fetch("https://learnwebcode.github.io/bootcamp-pet-data/pets.json")
     const petsData = await petsPromise.json()
     petsData.forEach(pet => {
-        console.log(pet.species)
+        const petCardClone = template.content.cloneNode(true)
+        petCardClone.querySelector("h3").textContent = pet.name
+
+        wrapper.appendChild(petCardClone)
     })
+    document.querySelector(".list-of-pets").appendChild(wrapper)
 }
 
 petsArea().then()
